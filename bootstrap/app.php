7aad1d40250ee8bfcd150b8c8cb188ffdfca1d2e
Mike Farrow <contact@mikefarrow.co.uk>
2015-05-05 14:35:10 +0100

    autodetecting timezone

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,47 @@
 
 /*
 |--------------------------------------------------------------------------
+| Timezone
+|--------------------------------------------------------------------------
+|
+| Provide a means to get the system timezone for mac/linux.
+| Not ideal and defaults to UTC if timezone can't be determined.
+| Placed here to move out of the way of config, sorry...
+|
+*/
+
+function laravel_timezone()
+{
+	$timezone = 'UTC';
+
+	if (is_link('/etc/localtime'))
+	{
+			$filename = readlink('/etc/localtime');
+			if (strpos($filename, '/usr/share/zoneinfo/') === 0)
+			{
+					$timezone = substr($filename, 20);
+			}
+	}
+	elseif (file_exists('/etc/timezone'))
+	{
+			$data = file_get_contents('/etc/timezone');
+			if ($data) {
+					$timezone = $data;
+			}
+	}
+	elseif (file_exists('/etc/sysconfig/clock'))
+	{
+			$data = parse_ini_file('/etc/sysconfig/clock');
+			if (!empty($data['ZONE'])) {
+					$timezone = $data['ZONE'];
+			}
+	}
+
+	return trim($timezone);
+}
+
+/*
+|--------------------------------------------------------------------------
 | Create The Application
 |--------------------------------------------------------------------------
 |
