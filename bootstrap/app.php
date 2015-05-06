67b01ceed9c6fbb99a4e20d1983ee996bf3841a1
Mike Farrow <contact@mikefarrow.co.uk>
2015-05-06 12:33:29 +0000

    making sure timezone function is only defined once

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,35 +10,37 @@
 | Placed here to move out of the way of config, sorry...
 |
 */
-
-function laravel_timezone()
+if ( ! function_exists('laravel_timezone'))
 {
-	$timezone = 'UTC';
+	function laravel_timezone()
+	{
+		$timezone = 'UTC';
 
-	if (is_link('/etc/localtime'))
-	{
-			$filename = readlink('/etc/localtime');
-			if (strpos($filename, '/usr/share/zoneinfo/') === 0)
-			{
-					$timezone = substr($filename, 20);
-			}
-	}
-	elseif (file_exists('/etc/timezone'))
-	{
-			$data = file_get_contents('/etc/timezone');
-			if ($data) {
-					$timezone = $data;
-			}
-	}
-	elseif (file_exists('/etc/sysconfig/clock'))
-	{
-			$data = parse_ini_file('/etc/sysconfig/clock');
-			if (!empty($data['ZONE'])) {
-					$timezone = $data['ZONE'];
-			}
-	}
+		if (is_link('/etc/localtime'))
+		{
+				$filename = readlink('/etc/localtime');
+				if (strpos($filename, '/usr/share/zoneinfo/') === 0)
+				{
+						$timezone = substr($filename, 20);
+				}
+		}
+		elseif (file_exists('/etc/timezone'))
+		{
+				$data = file_get_contents('/etc/timezone');
+				if ($data) {
+						$timezone = $data;
+				}
+		}
+		elseif (file_exists('/etc/sysconfig/clock'))
+		{
+				$data = parse_ini_file('/etc/sysconfig/clock');
+				if (!empty($data['ZONE'])) {
+						$timezone = $data['ZONE'];
+				}
+		}
 
-	return trim($timezone);
+		return trim($timezone);
+	}
 }
 
 /*
